501bee73c30b9849bd69dfe0bb64939f6f128913
Eyad-Mohammed-Osama <eyadbere@gmail.com>
2024-08-26 08:08:24 +0300

    Added DTOs

---
+++ src/DTO/LoginResponseData.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace EyadBereh\SyrianTaxIntegration\DTO;
+
+use Illuminate\Support\Str;
+use Spatie\LaravelData\Attributes\Computed;
+use Spatie\LaravelData\Data;
+
+class LoginResponseData extends Data
+{
+    #[Computed]
+    public readonly string $token;
+    public function __construct(
+        public readonly string $authorization_header,
+        public readonly string $facility_name,
+        public readonly int $response_code,
+    )
+    {
+        $this->token = Str::of($this->authorization_header)->after("Bearer")->trim();
+    }
+}
---
+++ src/DTO/SendInvoiceResponseData.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace EyadBereh\SyrianTaxIntegration\DTO;
+
+use Spatie\LaravelData\Data;
+
+class SendInvoiceResponseData extends Data
+{
+    public function __construct(
+        public readonly string $code,
+        public readonly string $datetime,
+        public readonly string $random_number,
+        public readonly string $response_code,
+    )
+    {
+    }
+}
