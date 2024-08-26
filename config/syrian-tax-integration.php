ac79b9980346bb33b7135496fb227534c8e6dc20
Eyad-Mohammed-Osama <eyadbere@gmail.com>
2024-08-26 08:08:32 +0300

    Added configuration keys

--- config/syrian-tax-integration.php
+++ config/syrian-tax-integration.php
@@ -1,6 +1,13 @@
 <?php
 
-// config for EyadBereh/SyrianTaxIntegration
 return [
-
+    'username' => env('SYRIAN_TAX_INTEGRATION_USERNAME'),
+    'password' => env('SYRIAN_TAX_INTEGRATION_PASSWORD'),
+    'tax_number' => env('SYRIAN_TAX_INTEGRATION_TAX_NUMBER'),
+    'program' => env('SYRIAN_TAX_INTEGRATION_PROGRAM'),
+    'queue' => [
+        'enabled' => true,
+        'connection' => env('QUEUE_CONNECTION'),
+        'name' => 'taxes'
+    ]
 ];
